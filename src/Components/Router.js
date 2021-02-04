eb4f98ec26fcf0a242c640e3b530246cae14f04e
bok <jkbok@irexnet.co.kr>
2021-02-04 13:37:06 +0900

    ...

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 // import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 
